886c39c9df68ab06699973f7926df46d07f09459
Enes AKIN <85414031+enesakn16@users.noreply.github.com>
2026-07-22 22:00:22 +0300

    feat: expose CSV reporting API

--- src/turkmopet_b2b/__init__.py
+++ src/turkmopet_b2b/__init__.py
@@ -1,9 +1,14 @@
+from .pipeline import AccountReport, build_reports, load_accounts, write_reports
 from .scoring import AccountTier, ScoreBreakdown, WholesaleAccount, rank_accounts, score_account
 
 __all__ = [
+    "AccountReport",
     "AccountTier",
     "ScoreBreakdown",
     "WholesaleAccount",
+    "build_reports",
+    "load_accounts",
     "rank_accounts",
     "score_account",
+    "write_reports",
 ]
